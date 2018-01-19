5ee02c4713af51d21ee99a9154ae2f88323752d8
leegor <leegor@126.com>
2018-01-19 23:50:14 +0800

    create Test1 project and firstly commit and push

---
+++ Test1/src/cn/com/zte/HelloWorld.java
@@ -0,0 +1,12 @@
+package cn.com.zte;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("hello world!");
+		for(int i=1;i<10;i++) {
+			System.out.println(i);
+		}
+	}
+
+}
